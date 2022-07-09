f475061f9b7a0f0424d5c8ac602dc612333b0769
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-07-09 18:26:45 +0900

    MainActivity改修

--- app/src/main/java/com/example/haircutremainder/MainActivity.kt
+++ app/src/main/java/com/example/haircutremainder/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.haircutremainder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.haircutremainder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnCount.setOnClickListener {
+            binding.tvCount.text = count++.toString()
+        }
+
+        binding.btnDownloadUserData.setOnClickListener {
+            downloadUserData()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..20000) {
+            Log.d("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
     }
 }
